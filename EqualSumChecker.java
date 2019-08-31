ebd5cbb379f359b39da899fc047bc5df0e762023
sneha2702 <49891480+sneha2702@users.noreply.github.com>
2019-08-31 12:52:31 -0500

    Add files via upload

---
+++ EqualSumChecker.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+public class EqualSumChecker {
+    public static boolean hasEqualSum(int x, int y, int z)
+    {
+        if(x+y==z)
+        {
+            return true;
+        }
+        return false;
+    }
+    
+}
---
+++ Main.java
@@ -0,0 +1,11 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+public class Main {
+    public static void main(String[] args) {
+        EqualSumChecker eqs= new EqualSumChecker();
+        eqs.hasEqualSum(1,-1,0);
+    }
+}
